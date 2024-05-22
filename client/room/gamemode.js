2dd17eea8ec5d3e93e8d0df6916d2ea53ed86f87
NIKTOR61yt <162542117+NIKTOR61yt@users.noreply.github.com>
2024-05-22 21:42:51 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -7,7 +7,6 @@ const EndOfMatchTime = 3;
 const GameStateValue = "Game";
 const EndOfMatchStateValue = "EndOfMatch";
 const EndAreaTag = "parcourend";
-const EndTriggerPoints = 1000;
 const CurSpawnPropName = "CurSpawn";
 const ViewEndParameterName = "ViewEnd";
 const LeaderBoardProp = "Leader";
@@ -15,17 +14,9 @@ const LeaderBoardProp = "Leader";
 const mainTimer = room.Timers.GetContext().Get("Main");
 var endAreas = room.AreaService.GetByTag(EndAreaTag);
 const stateProp = room.Properties.GetContext().Get("State");
-const inventory = room.Inventory.GetContext();
 const gnmeEndAreaColor = new basic.Color(0, 0, 1, 0);
 
-inventory.Main.Value = false;
-inventory.Secondary.Value = false;
-inventory.Melee.Value = false;
-inventory.Explosive.Value = false;
-inventory.Build.Value = false;
-
 const blueTeam = teams.create_team_blue();
-blueTeam.Spawns.RespawnTime.Value = 0;
 
 stateProp.OnValue.Add(OnState);
 function OnState() {
@@ -55,7 +46,6 @@ endTrigger.Tags = [EndAreaTag];
 endTrigger.Enable = true;
 endTrigger.OnEnter.Add(function (player) {
 	endTrigger.Enable = false;
-	player.Properties.Get(LeaderBoardProp).Value += 1000;
 	stateProp.Value = EndOfMatchStateValue;
 });
 
