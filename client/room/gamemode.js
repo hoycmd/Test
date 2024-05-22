bdf7026e5e81e6dfbf75090f43fdc40078d0f1bc
NIKTOR61yt <162542117+NIKTOR61yt@users.noreply.github.com>
2024-05-22 21:33:34 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -13,23 +13,35 @@ const LeaderBoardProp = "Leader";
 const mainTimer = room.Timers.GetContext().Get("Main");
 var endAreas = room.AreaService.GetByTag(EndAreaTag);
 const stateProp = room.Properties.GetContext().Get("State");
+const inventory = room.Inventory.GetContext();
 const gnmeEndAreaColor = new basic.Color(0, 0, 1, 0);
 
+inventory.Main.Value = false;
+inventory.Secondary.Value = false;
+inventory.Melee.Value = false;
+inventory.Explosive.Value = false;
+inventory.Build.Value = false;
+
 const blueTeam = teams.create_team_blue();
+blueTeam.Spawns.RespawnTime.Value = 0;
 
 stateProp.OnValue.Add(OnState);
 function OnState() {
 	const spawnsRoomContext = room.Spawns.GetContext();
 	switch (stateProp.Value) {
 		case GameStateValue:
+			spawnsRoomContext.enable = true;
 			break;
 		case EndOfMatchStateValue:
+			spawnsRoomContext.enable = false;
+			spawnsRoomContext.Despawn();
 			room.Game.GameOver(room.LeaderBoard.GetPlayers());
 			mainTimer.Restart(5);
+			break;
 	}
 }
 
-endTrigger.Tags = [EndAreaTag];
+@@ -56,28 +41,9 @@ endTrigger.Tags = [EndAreaTag];
 endTrigger.Enable = true;
 endTrigger.OnEnter.Add(function (player) {
 	endTrigger.Enable = false;
@@ -64,4 +76,5 @@ function InitializeMap() {
 }
 
 InitializeMap();
+
 stateProp.Value = GameStateValue;
