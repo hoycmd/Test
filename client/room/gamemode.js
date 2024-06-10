f18581a5a77f0040350c53f60201ad7c2fba1191
NIKTOR61yt <162542117+NIKTOR61yt@users.noreply.github.com>
2024-06-10 22:52:37 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -2,26 +2,18 @@ import * as basic from 'pixel_combats/basic';
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-var endAreas = room.AreaService.GetByTag("EndAreaTag");
 const gnmeEndAreaColor = new basic.Color(0, 0, 1, 0);
 
 const blueTeam = teams.create_team_blue();
 
-var endView = room.AreaViewService.GetContext().Get("EndView");
+var endView = room.AreaViewService.GetContext().Get("EndV");
 endView.Color = gnmeEndAreaColor;
 endView.Tags = ["EndAreaTag"];
 endView.Enable = true;
 
-const endTrigger = room.AreaPlayerTriggerService.Get("EndTrigger");
+const endTrigger = room.AreaPlayerTriggerService.Get("EndT");
 endTrigger.Tags = ["EndAreaTag"];
 endTrigger.Enable = true;
 
 room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); });
 room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn() });
-
-room.Map.OnLoad.Add(InitializeMap);
-function InitializeMap() {
-	endAreas = room.AreaService.GetByTag("EndAreaTag");
-}
-
-InitializeMap();
